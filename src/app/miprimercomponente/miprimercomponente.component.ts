24688bbd5399afa3d57419a1f69938938598470b
Veronica <veronica.ardilao@outlook.es>
2021-06-02 16:46:59 -0500

    se hizo la interpolacion de la etiqueta img, con la url del gif

--- src/app/miprimercomponente/miprimercomponente.component.ts
+++ src/app/miprimercomponente/miprimercomponente.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./miprimercomponente.component.css']
 })
 export class MiprimercomponenteComponent implements OnInit {
-
+  img = "https://thecatapi.com/api/images/get?format=src&type=gif?results_per_page=,"
   constructor() { }
 
   ngOnInit(): void {
